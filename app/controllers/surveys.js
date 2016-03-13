f82eaa25e5c41a5ccd8e84c1c116276eca56bb87
Ethan <erdesi90562@gmail.com>
2016-03-13 18:27:45 -0400

    create baseline for survey

---
+++ app/controllers/surveys.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const controller = require('lib/wiring/controller');
+const models = require('app/models');
+const Survey = models.survey;
+
+const authenticate = require('./concerns/authenticate');
+
+const index = (req, res, next) => {
+  Survey.find()
+    .then(surveys => res.json({ surveys }))
+    .catch(err => next(err));
+};
+
+const show = (req, res, next) => {
+  Survey.findById(req.params.id)
+    .then(survey => survey ? res.json({ survey }) : next())
+    .catch(err => next(err));
+};
+
+const create = (req, res, next) => {
+  let survey = Object.assign(req.body.survey, {
+    _owner: req.currentUser._id,
+  });
+  Survey.create(survey)
+    .then(survey => res.json({ survey }))
+    .catch(err => next(err));
+};
+
+module.exports = controller({
+  index,
+  show,
+  create,
+}, { before: [
+  { method: authenticate, except: ['index', 'show','create'], }
+], });
